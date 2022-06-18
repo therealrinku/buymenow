a3bcc4c73ecf657b145d8350a3db1e3e256d1f5d
Rinku Chaudhari <rinku.chaudhary@amniltech.com>
2022-06-18 17:30:45 +0545

    homepage initialized

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template:'<app-navbar></app-navbar>',
+  template:'<app-navbar></app-navbar><router-outlet>',
 })
 
 export class AppComponent {
